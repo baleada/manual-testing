caeab63b74cf4cc040d28f1f937e0c4ac24c177e
AlexVipond <alex.r.vipond@gmail.com>
2019-11-02 13:54:42 -0600

    identifiablegotrue

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,32 +1,50 @@
 <template>
   <main>
-    <button @click="handleClick">
+    <button @click="doStuff">
       do stuff
     </button>
+    <button @click="doOtherStuff">
+      do other stuff
+    </button>
+    <button @click="() => instance.login('baleada-example@alexvipond.dev', 'password')">
+      login
+    </button>
+    <pre><code>{{ responsesJson }}</code></pre>
+    <pre><code>{{ user }}</code></pre>
   </main>
 </template>
 
 <script>
 import { ref, computed, onMounted } from '@vue/composition-api'
-import useListenable from '~/assets/js/composition/useListenable'
+import useIdentifiableGoTrue from '~/assets/js/composition/useIdentifiableGoTrue'
 
 export default {
   setup() {
-    let listenable = {}
-    onMounted(() => {
-      listenable = useListenable('tap')
-      listenable.listen((event) => {
-        console.log(listenable.eventMetadata)
-      })
-    })
+    const userData = ref(null)
+    const instance = useIdentifiableGoTrue({
+            onGetUserData: data => (userData.value = data),
+            APIUrl: 'https://baleada.netlify.com/.netlify/identity',
+            setCookie: true,
+          }),
+          responsesJson = computed(() => JSON.stringify(instance.responses, null, 2)),
+          user = computed(() => JSON.stringify(instance.user, null, 2))
 
-    function handleClick() {
 
+    function doStuff () {
+      console.log(instance.userData)
+      console.log(instance)
+      // instance.login('baleada-example@alexvipond.dev', 'password')
+    }
+    function doOtherStuff () {
+      console.log(userData.value)
     }
 
     return {
-      listenable,
-      handleClick
+      instance,
+      responsesJson,
+      user,
+      doStuff,
+      doOtherStuff,
     }
   },
 }
