75f1df4a219028a2694fa96878ace5b7844e3d6e
AlexVipond <alex.r.vipond@gmail.com>
2020-04-24 00:44:55 -0600

    cookin with gas

--- nuxt/pages/completeable.vue
+++ nuxt/pages/completeable.vue
@@ -2,7 +2,7 @@
   <main class="p-8 bg-gray-8 h-screen w-screen flex flex-col items-center justify-center text-gray-4 space-x-4">
     <input 
       ref="baleada"
-      class="bg-gray-9 text-gray-4 p-4"
+      class="bg-gray-9 text-gray-4 p-4 w-full max-w-2"
       type="string"
       :value="completeable.string"
       @input="handleInput"
@@ -10,92 +10,99 @@
       @blur="handleBlur"
       @focus="handleFocus"
       @click="handleClick"
-      @keydown.up="handleArrow"
-      @keydown.right="handleArrow"
-      @keydown.down="handleArrow"
-      @keydown.left="handleArrow"
+      @keyup.up="handleArrow"
+      @keyup.right="handleArrow"
+      @keyup.down="handleArrow"
+      @keyup.left="handleArrow"
     />
-    <span class="h-em-1">{{ completeable.segment }}</span>
-    <pre class=""><code>{{ jsonSelection }}</code></pre>
-    <button type="button" @click="complete">complete</button>
+    <span class="mt-3 block w-full max-w-2 h-em-1">{{ completeable.segment }}</span>
+    <span class="mt-3 block w-full max-w-2 h-em-1">inputStatus: {{ inputStatus }}</span>
+    <span class="mt-3 block w-full max-w-2 h-em-1">valueStatus: {{ valueStatus }}</span>
+    <pre class="mt-3 block w-full max-w-2"><code>{{ jsonSelection }}</code></pre>
+    <button type="button" class="mt-3 border-4 border-gray-4 p-2 rounded-5" @click="complete">complete</button>
   </main>
 </template>
 
 <script>
-import { watchEffect, computed, ref } from '@vue/composition-api'
+import { watchEffect, watch, computed, ref } from '@vue/composition-api'
 import { useCompleteable } from '@baleada/vue-composition'
+
+function nextTick (callback) {
+  setTimeout(callback, 0)
+}
 
 export default {
   setup () {
-    const completeable = useCompleteable('Baleada: a toolkit for building web apps', { segment: { from: 'divider', to: 'divider' } }),
+    const completeable = useCompleteable('Baleada: a toolkit for building web apps', { segment: { from: 'selection', to: 'selection' } }),
           baleada = ref(null),
           jsonSelection = computed(() => JSON.stringify(completeable.value.selection)),
-          status = ref('ready')
+          inputStatus = ref('ready'), // ready|focused|blurred
+          valueStatus = ref('ready') // ready|selecting|selected
 
     function handleInput (event) {
       const { target: { value, selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
-
       completeable.value.setString(value)
       completeable.value.setSelection({ start, end, direction })
     }
     
     function handleSelect (event) {
-      const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
-
-      console.log(event)
-      
-      // Guard against infinite watch loop triggered by setSelectionRange
-      if (completeable.value.selection.start !== start || completeable.value.selection.end !== end) { 
+      switch (valueStatus.value) {
+      case 'ready':
+        // unreachable
+        break
+      case 'selecting':
+        // do nothing. This event was triggered programmatically, not by the user.
+        break
+      case 'selected':
+        const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
         completeable.value.setSelection({ start, end, direction })
+        break
       }
     }
 
     function handleClick (event) {
-      const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
-      status.value = 'focused'
-      completeable.value.setSelection({ start, end, direction })
+      switch (inputStatus.value) {
+      case 'ready':
+      case 'blurred':
+        inputStatus.value = 'focused'
+        completeable.value.setSelection({ start: 0, end: completeable.value.string.length, direction: 'none' })
+        break
+      case 'focused':
+        const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
+        completeable.value.setSelection({ start, end, direction })
+        break
+      }      
     }
 
     function handleArrow (event) {
-      // const {
-      //   target: {
-      //     selectionStart: start, 
-      //     selectionEnd: end, 
-      //     selectionDirection: direction
-      //   },
-      //   shiftKey,
-      // } = event
-
-      // if (!shiftKey) {
-      //   console.log('here')
-      //   completeable.value.setSelection({ start, end, direction })
-      //   console.log(completeable.value.selection)
-      // }
+      const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction }, shiftKey } = event
+      if (!shiftKey) { // Breaks for remapped keyboards :'(
+        completeable.value.setSelection({ start, end, direction })
+      }
     }
 
     function handleBlur () {
-      status.value = 'blurred'
+      inputStatus.value = 'blurred'
     }
 
     function handleFocus ({ target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } }) {
-      switch (status.value) {
+      switch (inputStatus.value) {
       case 'ready':
       case 'blurred':
-        status.value = 'focused'
-
-        switch (completeable.value.status) {
+        switch (valueStatus.value) {
+        case 'selecting':
+          // do nothing. This event was triggered programmatically, not by the user.
+          break
         case 'ready':
-        case 'set':
+        case 'selected':
+          inputStatus.value = 'focused'
           const { target: { selectionStart: start, selectionEnd: end, selectionDirection: direction } } = event
           completeable.value.setSelection({ start, end, direction })
-          break
-        case 'completed':
-          // do nothing. Watcher will set selection range after complete is called.
           break
         }
         break
       case 'focused':
-        //do nothing
+        // unreachable
         break
       }
     }
@@ -104,40 +111,33 @@ export default {
       completeable.value.complete('baleada', { newSelection: 'completion' })
     }
 
-    watchEffect(
+    watch(
+      () => completeable.value.selection.start + completeable.value.selection.end,
       () => {
         if (baleada.value !== null) {
-          switch (status.value) {
+          switch (inputStatus.value) {
           case 'ready':
           case 'blurred':
-            switch (completeable.value.status) {
-            case 'ready':
-            case 'set':
-              // do nothing
-              break
-            case 'completed':
-              baleada.value.focus()
-              baleada.value.setSelectionRange(
-                completeable.value.selection.start,
-                completeable.value.selection.end,
-                completeable.value.selection.direction,
-              )
-            }
+            // Completeable was changed by a button click
+            valueStatus.value = 'selecting'
+            baleada.value.focus()
+            baleada.value.setSelectionRange(
+              completeable.value.selection.start,
+              completeable.value.selection.end,
+              completeable.value.selection.direction,
+            )
+            nextTick(() => (valueStatus.value = 'selected'))
             break
           case 'focused':
-            switch (completeable.value.status) {
-            case 'ready':
-            case 'set':
-              baleada.value.setSelectionRange(
-                completeable.value.selection.start,
-                completeable.value.selection.end,
-                completeable.value.selection.direction,
-              )
-              break
-            case 'completed':
-              // do nothing
-              break
-            }
+            // Completeable was changed by an input event or a keycombo
+            valueStatus.value = 'selecting'
+            baleada.value.setSelectionRange(
+              completeable.value.selection.start,
+              completeable.value.selection.end,
+              completeable.value.selection.direction,
+            )
+            nextTick(() => (valueStatus.value = 'selected'))
+            break
           }
         }
       }
@@ -154,6 +154,8 @@ export default {
       handleClick,
       handleFocus,
       handleArrow,
+      inputStatus,
+      valueStatus,
     }
   }
 }
